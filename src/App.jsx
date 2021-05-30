c88047330c711a444dd5bdbbbb10fe77b2901b45
cyrogabrielcc <cyrocunhacg@gmail.com>
2021-05-30 14:15:20 -0300

    +

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import './App.css'
+
+export default () => {
+  return (
+    <div className="app-window">
+        <div className="sidebar">
+            <header>
+
+            </header>
+            
+            <div className="search">
+
+            </div>
+
+            <div className="chatlist">
+              
+            </div>
+        </div>
+
+        <div className="contentarea" >
+            área de conteúdo
+        </div>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
