20a6be026d2549faa90905103057e15edb00efb7
cyrogabrielcc <cyrocunhacg@gmail.com>
2021-06-03 09:42:44 -0300

    +

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,5 +1,17 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react'
+import './ChatWindow.css'
+
+
+/* import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+    import ChatIcon from '@material-ui/icons/Chat';
+    import MoreVertIcon from '@material-ui/icons/MoreVert';
+    
+ */
+import SearchIcon from '@material-ui/icons/Search';
+import AttachFiletIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 
 export default() => {
     return (
@@ -9,6 +21,14 @@ export default() => {
                     <img src="https://www.w3schools.com/w3images/avatar2.png" alt="" className="chatWindow--avatar" />
                     <div className="chatWindow--name">Cyro Gabriel</div>
                 </div>
+            <div className="chatWindow--headerButtons">
+                <div className="chatWindow--Btn">
+                    <SearchIcon style={{color:'#919191'}}/>
+                    <AttachFiletIcon style={{color:'#919191'}}/>
+                    <MoreVertIcon style={{color:'#919191'}}/>
+                </div>
+            </div>
+
             </div>
             <div className="chatWindow--body"></div>
             <div className="chatWindow--footer"></div>
